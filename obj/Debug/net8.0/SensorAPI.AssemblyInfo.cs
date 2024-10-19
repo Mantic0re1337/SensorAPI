a93d887fce1d78a7745aa710a61b38fd257dc1e0
Maik <maik.wilke1997@web.de>
2024-10-19 20:49:13 +0200

    testing the key permantly

--- obj/Debug/net8.0/SensorAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SensorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SensorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857f6c1c4d9edaf8931b7d59c125f6ef82ab5365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe4bd581803aed0e8e91d415b9166b4306f6669")]
 [assembly: System.Reflection.AssemblyProductAttribute("SensorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SensorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
